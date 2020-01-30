158d6e04326e2841c722a8e7608ab5c18368a3c7
Wilson Moura <wilsonsmoura@gmail.com>
2020-01-30 18:42:25 -0300

    Fixed #6 -- Cookie generator

--- impact-measurement.php
+++ impact-measurement.php
@@ -13,7 +13,7 @@
  * @package           Impact_Measurement
  *
  * @wordpress-plugin
- * Plugin Name:       Impact Measurement plugin for WordPress
+ * Plugin Name:       Impact Measurement
  * Plugin URI:        https://github.com/bireme/impact-measurement-wp-plugin/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
@@ -36,6 +36,13 @@ if ( ! defined( 'WPINC' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define( 'IMPACT_MEASUREMENT_VERSION', '1.0.0' );
+define( 'IMPACT_MEASUREMENT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'IMPACT_MEASUREMENT_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+defined('IMPACT_MEASUREMENT_API') or define('IMPACT_MEASUREMENT_API', 'http://127.0.0.1:8000/api/main/?format=json&code=');
+defined('IMPACT_MEASUREMENT_COOKIE_DOMAIN_SCOPE') or define('IMPACT_MEASUREMENT_COOKIE_DOMAIN_SCOPE', '/');
+defined('IMPACT_MEASUREMENT_BVS_COOKIE_DOMAIN') or define('IMPACT_MEASUREMENT_BVS_COOKIE_DOMAIN', '');
+defined('IMPACT_MEASUREMENT_BVSALUD_COOKIE_DOMAIN') or define('IMPACT_MEASUREMENT_BVSALUD_COOKIE_DOMAIN', '');
+defined('IMPACT_MEASUREMENT_BIREME_COOKIE_DOMAIN') or define('IMPACT_MEASUREMENT_BIREME_COOKIE_DOMAIN', '');
 
 /**
  * The code that runs during plugin activation.
@@ -79,4 +86,5 @@ function run_impact_measurement() {
 	$plugin->run();
 
 }
+
 run_impact_measurement();
